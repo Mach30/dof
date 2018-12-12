b891847eb8c0831ad4d09fdf33333c6179ea4e28
J. Simmons <jrs@mach30.org>
2018-12-11 22:58:22 -0700

    First pass at fixing paths in buildDocs.js.  Adds command line arguments to override default options and a required template file name.

--- scripts/buildDocs.js
+++ scripts/buildDocs.js
@@ -3,14 +3,53 @@ var fs   = require('fs');
 var Liquid = require('liquidjs');
 var engine = Liquid();
 
-//TODO - fix path
-var architecturePath = './architecture/';
+var argv = require('minimist')(process.argv.slice(2));
+
+// declare configurable variables with default values
+var projectPath = process.cwd();
+var architectureDirectoryName = 'architecture';
+var outputDirName = 'dist';
+var unifiedModelFile = 'architecture.yaml'
+var templateFileName = '';
+
+// handle arguments
+if (argv['h'] || argv['help']) {  // if asked for help, print it and exit
+    console.log('Usage: node buildDocs.js [options] <template_file>');
+    console.log('--help, -h             Print the usage message and exit');
+    console.log('--projectPath=<Path/To/Project/>                     (default=CWD)');
+    console.log('--archDirName=<directory_with_architecture_models>   (default=architecture)');
+    console.log('--outputDirName=<directory_to_write_output_files>    (default=dist');
+    console.log('--unifiedModelFile=<filename_of_unified_model>       (default=architecture.yaml');
+    process.exit();
+} else { // otherwise process arguments
+    if ('projectPath' in argv) 
+        projectPath = argv['projectPath'];
+
+    if ('archDirName' in argv)
+        architectureDirectoryName = argv['archDirName'];
+    
+    if ('outputDirName' in argv)
+        outputDirName = argv['outputDirName'];
+    
+    if ('unifiedModelFile' in argv)
+        unifiedModelFile = argv['unifiedModelFile'];
+}
+
+if ('_' in argv) {
+    templateFileName = argv['_'][0];
+} else {
+    console.log('Must supply template file name');
+    process.exit(1);
+}
+
+
+var architecturePath = projectPath + '/' + architectureDirectoryName;
 var architectureContents = '';
 
 // load architecture data (package.json, ./architecture content)
 var architecture = {};
 architecture.elements = {};
-architecture.package = require('../package.json');
+architecture.package = JSON.parse(fs.readFileSync(projectPath + '/package.json', 'utf8'));
 architecture.generationDate = new Date().toLocaleDateString("en-US", {
     "year": "numeric",
     "month": "numeric",
@@ -34,16 +73,13 @@ try {
     console.log(e);
 }
 
-//TODO - fix path
-fs.writeFileSync('./dist/architecture.yaml', yaml.safeDump(architecture));
+fs.writeFileSync(projectPath + '/' + outputDirName + '/' + unifiedModelFile, yaml.safeDump(architecture));
 
-//TODO - fix path 
-var READMEtemplate = fs.readFileSync('./README.adoc.liquid', 'utf8');
+var templateFile = fs.readFileSync(projectPath + '/' + templateFileName, 'utf8');
 engine
-    .parseAndRender(READMEtemplate, {architecture: architecture})
+    .parseAndRender(templateFile, {architecture: architecture})
     .then(function(fulfilled) {
-        //TODO - fix path
-        fs.writeFileSync('./README.adoc', fulfilled);
+        fs.writeFileSync(projectPath + '/' + templateFileName.replace('.liquid', ''), fulfilled);
     }).catch(function(e) {
         console.log(e);
     });
