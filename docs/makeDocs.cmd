7acfaa7f4e821e7766a356863188b4733300bd70
J. Simmons <jrs@mach30.org>
2019-01-01 12:49:44 -0700

    Create first draft of docs

--- docs/makeDocs.cmd
+++ docs/makeDocs.cmd
@@ -2,4 +2,4 @@ REM build unified model
 node ../scripts/buildUnifiedModel.js --archDirName=specs --outputDirName=. --unifiedModel=spec.yaml
 
 REM generate architecture file formats doc
-REM TODO
+node ../scripts/generateDoc.js --unifiedModel=spec.yaml --template=README.adoc.liquid --out=README.adoc
--- docs/makeDocs.sh
+++ docs/makeDocs.sh
@@ -4,4 +4,4 @@
 node ../scripts/buildUnifiedModel.js --archDirName=specs --outputDirName=. --unifiedModel=spec.yaml
 
 # generate architecture file formats doc
-# TODO
+node ../scripts/generateDoc.js --unifiedModel=spec.yaml --template=README.adoc.liquid --out=README.adoc
