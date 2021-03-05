17857345d6a0e3b96cf070f05361b759aa4e0fee
J. Simmons <simmons@j-dot.org>
2021-03-05 14:11:29 -0800

    Move docs/ to https://github.com/Mach30/m30ml

--- docs/makeDocs.cmd
+++
@@ -1,5 +0,0 @@
-REM build unified model
-node ../scripts/buildUnifiedModel.js --archDirName=specs --outputDirName=. --unifiedModel=spec.yaml
-
-REM generate architecture file formats doc
-node ../scripts/generateDoc.js --unifiedModel=spec.yaml --template=README.adoc.liquid --out=README.adoc
--- docs/makeDocs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# build unified model
-node ../scripts/buildUnifiedModel.js --archDirName=specs --outputDirName=. --unifiedModel=spec.yaml
-
-# generate architecture file formats doc
-node ../scripts/generateDoc.js --unifiedModel=spec.yaml --template=README.adoc.liquid --out=README.adoc
